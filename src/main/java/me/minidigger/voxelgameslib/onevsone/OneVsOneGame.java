18a8ad375ac0d5eea210af21f4d79b7a9f285312
MiniDigger <admin@minidigger.me>
2017-06-18 17:51:43 +0200

    move up world loading code

--- src/main/java/me/minidigger/voxelgameslib/onevsone/OneVsOneGame.java
+++ src/main/java/me/minidigger/voxelgameslib/onevsone/OneVsOneGame.java
@@ -1,26 +1,19 @@
 package me.minidigger.voxelgameslib.onevsone;
 
-import java.util.Optional;
 import javax.annotation.Nonnull;
-import javax.inject.Inject;
 
 import me.minidigger.voxelgameslib.game.AbstractGame;
 import me.minidigger.voxelgameslib.game.GameDefinition;
 import me.minidigger.voxelgameslib.game.GameInfo;
-import me.minidigger.voxelgameslib.map.MapInfo;
 import me.minidigger.voxelgameslib.phase.phases.GracePhase;
 import me.minidigger.voxelgameslib.phase.phases.LobbyPhase;
 import me.minidigger.voxelgameslib.phase.phases.VotePhase;
-import me.minidigger.voxelgameslib.world.WorldHandler;
 
 /**
  * Created by Martin on 28.01.2017.
  */
 @GameInfo(name = "1vs1", author = "MiniDigger", version = "v1.0", description = "1vs1 Description")
 public class OneVsOneGame extends AbstractGame {
-
-    @Inject
-    private WorldHandler worldHandler;
 
     public OneVsOneGame() {
         super(OneVsOnePlugin.GAMEMODE);
@@ -50,16 +43,5 @@ public class OneVsOneGame extends AbstractGame {
         super.initGameFromDefinition(gameDefinition);
 
         loadMap();
-    }
-
-    private void loadMap() {
-        //TODO move this somewhere else (unify)
-        // TODO this doesn't respect if a user changed the lobby in the config
-        Optional<MapInfo> info = worldHandler.getMapInfo("Lobby");
-        if (info.isPresent()) {
-            putGameData("lobbymap", info.get());
-        } else {
-            abortGame();
-        }
     }
 }
