45359c63fda8fe79806b3a9d00de06d6f4e6e39e
MiniDigger <admin@minidigger.me>
2018-02-11 12:42:29 +0100

    add all projects to travis

---
+++ scripts/deploy-build-results.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "clean deploy folder"
+rm -rf deploy-stuff
+
+git clone --depth 10 -b gh-pages "https://${GITHUB_TOKEN}@github.com/VoxelGamesLib/VoxelGamesLibv2.git" deploy-stuff
+
+# config
+echo "setup git"
+git config --global user.email "vglbot@minidigger.me"
+git config --global user.name "VoxelGamesLibBot"
+
+# copy over stuff we want to deploy
+echo "copy stuff to deploy"
+cp -R build/dependencyUpdates/. deploy-stuff/1vs1
+cp -R build/docs/javadoc/. deploy-stuff/1vs1
+cp -R build/reports/. deploy-stuff/1vs1
+cp -R build/libs/. deploy-stuff/1vs1
+
+# create mvn repo
+mkdir deploy-stuff/mvn-repo/
+mvn deploy:deploy-file -Dfile=build/libs/1vs1-1.0-SNAPSHOT.jar -DpomFile=pom.xml  -Durl=file://${TRAVIS_BUILD_DIR}/deploy-stuff/mvn-repo
+
+# deploy
+echo "commit repo"
+cd deploy-stuff
+git add .
+echo "commit"
+git commit -m "Deploy to Github Pages (1vs1)"
+echo "push"
+git push --force "https://${GITHUB_TOKEN}@github.com/VoxelGamesLib/VoxelGamesLibv2.git" gh-pages:gh-pages
