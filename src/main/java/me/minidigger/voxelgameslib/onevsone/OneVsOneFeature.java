fcfe7bc724ea0b7db082c8fdc8b1efc9654f5709
Sxtanna <ranald.taylor@gmail.com>
2018-02-14 19:07:50 -0500

    Updated to use strongly typed dependencies

--- src/main/java/me/minidigger/voxelgameslib/onevsone/OneVsOneFeature.java
+++ src/main/java/me/minidigger/voxelgameslib/onevsone/OneVsOneFeature.java
@@ -1,9 +1,12 @@
 package me.minidigger.voxelgameslib.onevsone;
 
+import java.util.Collections;
+import java.util.List;
 import javax.annotation.Nonnull;
 
 import com.voxelgameslib.voxelgameslib.event.GameEvent;
 import com.voxelgameslib.voxelgameslib.feature.AbstractFeature;
+import com.voxelgameslib.voxelgameslib.feature.Feature;
 import com.voxelgameslib.voxelgameslib.feature.features.DuelFeature;
 
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -38,7 +41,7 @@ public class OneVsOneFeature extends AbstractFeature {
 
     @Override
     @Nonnull
-    public Class[] getDependencies() {
-        return new Class[]{DuelFeature.class};
+    public List<Class<? extends Feature>> getDependencies() {
+        return Collections.singletonList(DuelFeature.class);
     }
 }
